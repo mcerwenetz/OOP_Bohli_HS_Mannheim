9c846c691112871fc955357e4b183c82835f310f
swt <swt@pb22>
2022-09-28 21:33:58 +0200

    ub1a1 recursive

--- Uebungsblaetter/UB1_A1/main.cpp
+++ Uebungsblaetter/UB1_A1/main.cpp
@@ -9,43 +9,33 @@
 #include <stdio.h>
 #include "ooptool.h"
 
+double heron_calc(double heron, double radizent, double accuracy){
+	double heron_next = (heron+(radizent/heron))/2;
+	if(heron- heron_next < accuracy){
+		return heron;
+	}
+	else
+	{
+		return heron_calc(heron_next, radizent, accuracy);
+	}
+}
+
 
 int main(int argc, const char* argv[])
 {
 	argsp_t argsp(argc, argv);
 	double radizent = argsp.double_pos(0, 10.0);
 	double soll_abstand = argsp.double_pos(1, 1e-6);
-	double abstand = 1.0;
 	double heron = 0.0;
-	double heron_plus_one=1;
 
-	for(int i=0; abstand > soll_abstand; i++)
-	{
-		/* Im ersten Durchlauf wird das erste heron-glied noch anders berechnet.
-		 * Spätere Heron-Glieder bauen auf dem ersten auf.
-		 * Umsetzung als Rekursiver Algorithmus bietet sich hier an. Hatte aber keinen Bock drauf.
-		 */
-		if(i==0)
-		{
-			heron=(1+radizent)/2;
-		}
-		else
-		{
-			/*
-			 * Für spätere Heron-Glieder gilt das dieser Else-Case.
-			 * Wichtig ist hier die Reihenfolge:
-			 * 1.Heronglied berechnen
-			 * 2.Abstand berechnen zum vorherigen berechnen.
-			 * 3.Nächstes Heronglied bekommt den Wert des Aktuellen.
-			 * Berechnet man zuletzt den Abstand ist dieser immer 0, weil man vom heron-glied einfach das aktuelle heron-glied abzieht.
-			 */
-			heron=((heron+(radizent/heron))/2);
-			abstand=heron_plus_one-heron;
-		}
-	heron_plus_one=heron;
-	}
+	heron = (1 + radizent) /2;
+	heron = heron_calc(heron, radizent, soll_abstand);
+
 	//Ausgabe als float.
 	printf("%lf",heron);
 	return 0;
+
 }
+
+
 
